0d987d7361beaf65419b422bf5aa6852665a426b
Billy-Ugalde <billyugalde2004@gmail.com>
2025-09-14 19:40:13 -0600

    Interfaces de reportes de ferias

---
+++ src/modules/fairs/interfaces/report.interface.ts
@@ -0,0 +1,10 @@
+export interface IReportFairService {
+  createReport(params: ReportFairParams): Promise<Buffer>; 
+  getReportByFair()
+  getReportFair()
+}
+
+ interface ReportFairParams {
+  year: number;              // Ej: 2025
+  quarter: 1 | 2 | 3 | 4;    // Q1..Q4
+}
