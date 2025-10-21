d0876ba3e51bd4b74053c506451a175a2b58eed3
Billy-Ugalde <billyugalde2004@gmail.com>
2025-10-21 15:06:54 -0600

    Ajustes menores

--- src/modules/volunteers/controllers/mailbox.controller.ts
+++ src/modules/volunteers/controllers/mailbox.controller.ts
@@ -7,7 +7,9 @@ import {
   Body,
   ParseIntPipe,
   UseInterceptors,
-  UploadedFiles
+  UploadedFiles,
+  HttpCode,
+  HttpStatus
 } from "@nestjs/common";
 import { FileFieldsInterceptor } from "@nestjs/platform-express";
 import { MailboxService } from "../services/mailbox.service";
@@ -15,13 +17,21 @@ import { CreateMailboxDto } from "../dto/createMailbox.dto";
 import { UpdateMailboxDto } from "../dto/updateMailbox.dto";
 import { MailboxFiles } from "../interfaces/mailbox.interface";
 import { ParseJsonFieldsInterceptor } from "src/common/interceptors/parse-json-fields.interceptor";
+import { AuthGuard } from "src/modules/auth/guards/auth.guard";
+import { RoleGuard } from "src/modules/auth/guards/role.guard";
+import { Roles } from "src/modules/auth/decorators/roles.decorator";
+import { UserRole } from "src/modules/auth/enums/user-role.enum";
 
 
 @Controller('mailbox')
+// @UseGuards(AuthGuard)
 export class MailboxController {
   constructor(private readonly mailboxService: MailboxService) { }
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
+  // @UseGuards(RoleGuard)
+  //@Roles(UserRole.VOLUNTEER, UserRole.SUPER_ADMIN, UserRole.GENERAL_ADMIN)
   @UseInterceptors(
     FileFieldsInterceptor([
       { name: 'documents', maxCount: 3 }
@@ -39,6 +49,8 @@ export class MailboxController {
   }
 
   @Put(':id')
+  // @UseGuards(RoleGuard)
+  // @Roles(UserRole.SUPER_ADMIN, UserRole.GENERAL_ADMIN)
   @UseInterceptors(
     FileFieldsInterceptor([
       { name: 'Document1_file', maxCount: 1 },
@@ -57,11 +69,15 @@ export class MailboxController {
   }
 
   @Get()
+  // @UseGuards(RoleGuard)
+  // @Roles(UserRole.SUPER_ADMIN, UserRole.GENERAL_ADMIN, UserRole.AUDITOR)
   async getAllMailbox() {
     return await this.mailboxService.getAllMailbox();
   }
 
   @Get(':id')
+  // @UseGuards(RoleGuard)
+  // @Roles(UserRole.SUPER_ADMIN, UserRole.GENERAL_ADMIN, UserRole.AUDITOR)
   async getMailboxById(@Param('id', ParseIntPipe) id: number) {
     return await this.mailboxService.getMailboxById(id);
   }
