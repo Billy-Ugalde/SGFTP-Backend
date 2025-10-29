f23ee9d665217fe8a89870c6ac127b60b85acc21
Billy-Ugalde <billyugalde2004@gmail.com>
2025-10-28 20:54:48 -0600

    Correciones

--- src/modules/projects/project.module.ts
+++ src/modules/projects/project.module.ts
@@ -9,7 +9,6 @@ import { GoogleDriveService } from "../google-drive/google-drive.service";
 import { ActivityController } from "./controllers/activity.controller";
 import { ActivityService } from "./services/activity.service";
 import { ReportProjectController } from "./controllers/reportProject.controller";
-import { ReportActivityController } from "./controllers/reportActivity.controller";
 import { ReportProjectService } from "./services/reportProject.service";
 import { ReportActivityService } from "./services/reportActivity.service";
 import { Metric_value } from "./entities/activityValues.entity";
@@ -18,7 +17,7 @@ import { Activity_enrollment } from "../volunteers/entities/enrollmentActivity.e
 
 @Module({
     imports: [TypeOrmModule.forFeature([Project, Activity, DateActivity, Metric_value, Activity_enrollment]), AuthModule],
-    controllers: [ProjectController, ActivityController, ReportProjectController, ReportActivityController],
+    controllers: [ProjectController, ActivityController, ReportProjectController, ActivityController],
     providers: [ProjectService, ActivityService, GoogleDriveService, ReportProjectService, ReportActivityService]
 })
 export class ProjectModule { }
